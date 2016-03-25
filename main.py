d0919ce642313cde9520a547135d9da8e47ae88d
Connor Hillen <connor.hillen@gmail.com>
2016-03-25 19:47:36 -0400

    Getting things started

---
+++ main.py
@@ -0,0 +1,16 @@
+def headprint(s):
+    print(" # "+s+" # \n")
+
+def Decision_Tree():
+    headprint("Decision Tree")
+    
+def Optimal_Bayesian():
+    headprint("Optimal Bayesian")
+
+def Naive_Bayesian():
+    headprint("Naive Bayesian")
+    
+def Linear_Bayesian():
+    headprint("Linear Bayesian")
+    
+Optimal_Bayesian();
