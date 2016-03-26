07835611e8e1674bbe0af8f698b83e51c646353c
Connor Hillen <connor.hillen@gmail.com>
2016-03-25 20:10:09 -0400

    Adding some comments Felt like inserting some guides for future coding. Just TODOs, really. Will be removed eventually.

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ def headprint(s):
 # BEGIN MAIN ALGORITHMS 
 def Decision_Tree():
     headprint("Decision Tree")
-    
-def Optimal_Bayesian():
-    headprint("Optimal Bayesian")
 
+## 1. Partition the training data into two sets, belonging to class A and class B
+## 2. Calculate normal distribution for each set
+## 3. Form the classification equation
 def Naive_Bayesian():
     headprint("Naive Bayesian")
-    
+   
+## I assume this is just a single vector as opposed to matrix (i.e. not diagonal)? Not entirely sure.        
 def Linear_Bayesian():
     headprint("Linear Bayesian")
-    
+        
+## The only difference between OB and NB is the covariance matrix used. In Naive, the 
+## covariance matrix is just a diagonal matrix, but in an optimal bayes we use a full
+## matrix.        
+def Optimal_Bayesian():
+    headprint("Optimal Bayesian")
