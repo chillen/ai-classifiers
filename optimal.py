13099561fdd378820dad3d9b2d6b245d7c7dd348
Connor Hillen <connor.hillen@gmail.com>
2016-03-31 06:21:47 -0400

    More stable, but getting towards jank

--- optimal.py
+++ optimal.py
@@ -1,11 +1,13 @@
 import numpy as np
 from collections import defaultdict
+import math
 
-def _build(S, headers):
-    splitArrays = np.array_split(S, 2)
-    training = splitArrays[0]
-    testing = splitArrays[1]
-    
+def getClassValue(Ea, Eb, Ma, Mb, x):
+    mbChunk = np.multiply(np.transpose((np.subtract(x, Mb))), np.multiply(np.linalg.pinv(Eb), np.subtract(x,Mb)))
+    maChunk = np.multiply(np.transpose((np.subtract(x, Ma))), np.multiply(np.linalg.pinv(Ea), np.subtract(x,Ma)))
+    return log(Eb) - log(Ea) + mbChunk - maChunk
+
+def getClassifier(trainingSet):
     T = defaultdict(lambda: [])
     for sample in training:
         c = sample[0]
@@ -13,10 +15,8 @@ def _build(S, headers):
 
     sums = {}
     M = {}
-    k = 0
     
     for key in T:
-        k = len(T[key][0]) - 1 # store the length for later
         sums[key] = []
         for sample in T[key]:
             for i, feature in enumerate(sample[1:]):
@@ -43,8 +43,48 @@ def _build(S, headers):
     
     for key in T:
         E[key] = (1/(len(T[key])-1)) * sums[key]
+    return {"E": E, "M": M}
+
+def _build(S, headers):
+    splitArrays = np.array_split(S, 2)
+    training = splitArrays[0]
+    testing = splitArrays[1]
+    
+    T = defaultdict(lambda: [])
+    for sample in training:
+        c = sample[0]
+        T[c].append(sample)
+
+    sums = {}
+    M = {}
+    
+    for key in T:
+        sums[key] = []
+        for sample in T[key]:
+            for i, feature in enumerate(sample[1:]):
+                if len(sums[key]) > i:
+                    sums[key][i] += feature
+                else:
+                    sums[key].append(feature)
+    for key in sums:
+        M[key] = []
+        for element in sums[key]:
+           M[key].append(element/len(T[key])) 
+    
+    # Mean vectors calculated, find the sum of (xi - Ma)(xi-Ma)^T
+    sums = {}
+    for key in T:
+        summable = []
+        for x in [sample[1:] for sample in T[key]]:
+            x1 = np.subtract(x, M[key])
+            x2 = np.transpose(x1)
+            summable.append( np.outer(x1,x2) )
+        sums[key] = np.sum(summable, axis=0)
         
-    print(E)
+    E = {}
+    
+    for key in T:
+        E[key] = (1/(len(T[key])-1)) * sums[key]
                 
 
 def build(S, headers):
